eab01792af20a0f75633bd603831f034e8b7c0db
Ryan Barrar <Rbarrar17@gmail.com>
2016-06-16 18:25:31 -0400

    learned how to input parameters an arguments in functions

---
+++ php/functions_part_2/parameters_and_arguments.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+      <p>
+        <?php
+        function greetings($name){
+            echo "Greetings, ".$name."!";
+        }
+
+        greetings('Ryan');
+        ?>
+      </p>
+    </body>
+</html>
