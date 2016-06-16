06734991c7b48bf4378c3de945a9394ac02968a2
Ryan Barrar <Rbarrar17@gmail.com>
2016-06-16 18:20:52 -0400

    first php function

---
+++ php/functions_part_2/your_first_function.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+      <p>
+        <?php
+        // Write your function below!
+        function displayName(){
+            echo "Ryan";
+        }
+
+        displayName();
+        ?>
+      </p>
+    </body>
+</html>
